64a44bb086150ff046abe1b37d5e9c7a474f6fb9
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-03 10:06:33 +1000

    copy, cut, paste

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-copy = word[0:3]
-cut = word[7:9]
-paste = word[1:8]
+copy = word[:3]
+cut = word[-2:]
+paste = word[1:-1]
 print(f"First 3 letters: {copy}")
 print(f"Last 2 letters: {cut}")
 print(f"Middle word: {paste}")
