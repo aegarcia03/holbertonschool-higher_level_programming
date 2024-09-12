bc9a6601ad084a61051a409fd9b10e8512f62e86
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-12 12:13:21 +1000

    prints only integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     idx = 0
     for i in range(x):
         try:
-            if isinstance(my_list[i], int) and idx < x:
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end='')
                 idx += 1
         except TypeError:
