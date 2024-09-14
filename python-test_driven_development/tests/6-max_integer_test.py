925faaa922252e142c4be2c3e53983b5dd381077
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-14 19:50:50 +1000

    unittest

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+class TestmaxInteger(unittest.TestCase):
+
+    def test
+
+
+if __name__ == '__main__':
+    unittest.main()
