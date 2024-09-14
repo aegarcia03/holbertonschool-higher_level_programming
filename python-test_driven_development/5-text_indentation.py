b1f4fd8154e51d7e271c1c27ede01982e5e5bd72
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-14 12:35:30 +1000

    Text Indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Prints a text with 2 lines after each of these characters: '.', '?', ':'"""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.' '?' ':'
+
+    Parameters:
+    text: a string.
+
+    Raises:
+    TypeError: if text is not a string.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    container = []
+    i = 0
+
+    while i < len(text):
+        if text[i] == '.' or text[i] == ':' or text[i] == '?':
+            container.append(text[i])
+            container.append('\n\n')
+
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        else:
+            container.append(text[i])
+        i += 1
+    result = ''.join(container).strip()
+    print(result)
