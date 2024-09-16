ac3962bd2f21a90046ea579488348a64d562ba5d
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-16 17:52:23 +1000

    size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """ A class used to define a square"""
+
+    def __init__(self, size=0):
+        """Initialize the square with a private instance attribute
+
+        Args:
+        size: The size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
