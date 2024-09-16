01313d7f7986c8ee80f455fdf56891a29cc2f229
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-16 18:28:23 +1000

    Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """ A class used to define a square"""
+
+    def __init__(self, size=0):
+        """Initialize the square with a private instance attribute
+
+        Args:
+        size: The size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
