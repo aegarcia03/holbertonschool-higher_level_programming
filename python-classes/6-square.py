4beb10b06093936b9c6561538babdc10b4a3dec2
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-16 20:00:19 +1000

    coordinates of a square

---
+++ python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """ A class used to define a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the square with a private instance attribute
+
+        Args:
+        size: The size of the square
+        position: position of the square, must be a tuple
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+
+        if(not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value)
+                or not all(num >= 0 for num in value)):
+            raise TypeError(
+                    "position must be a tuple of 2 positive integers"
+                    )
+        self.__position = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints a square pattern with the character #"""
+        if self.__size == 0:
+            print()
+
+        for i in range(0, self.position[0]):
+            for j in range(0, self.__size):
+                print("#", end='')
+            print()
