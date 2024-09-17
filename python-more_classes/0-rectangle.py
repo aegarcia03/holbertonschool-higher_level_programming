5c35b3488559a2213102bdf6b603ebd1d9e6376a
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-17 11:36:43 +1000

    class rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Rectangle"""
+
+
+class Rectangle:
+    """
+    A class used to define a rectangle
+
+    Attributes: ...
+    ------------
+    Methods: ...
+    ---------
+    """
+    pass
