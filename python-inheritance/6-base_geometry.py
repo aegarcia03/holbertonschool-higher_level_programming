4adaf6a442558ac7fb89e09c8018755ed7786cf3
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-23 16:00:46 +1000

    improve geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a geometry module"""
+
+
+class BaseGeometry:
+    """
+    Is a class of Geometry
+
+    Atributes
+        None
+    """
+
+    def area(self):
+        """Raises an Exception with a message"""
+        raise Exception("area() is not implemented")
