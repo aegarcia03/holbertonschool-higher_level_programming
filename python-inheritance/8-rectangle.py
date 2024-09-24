25bb34249763251e48550f0d4e254d5a0c42932b
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-24 12:47:56 +1000

    rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a geometry module"""
+
+
+class BaseGeometry:
+    """
+    Is a class of Geometry
+
+    Atributes
+        None
+    """
+
+    def area(self):
+        """Raises an Exception with a message"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value as an integer
+
+        Args:
+            name(str): The name of the parameter
+            value(int): The parameter to validate
+        Raises:
+            TypeError if value is not an integer
+            ValueError if value not greater than zero
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    Is a subclass of BaseGeometry
+
+    Atributes
+        None
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
