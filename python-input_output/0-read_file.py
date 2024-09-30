6d740a5c82a3b494c0a150cb884dc7e0e0d23906
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-30 14:50:44 +1000

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function"""
+
+
+def read_file(filename=""):
+    """Print the content of a UTF8 file in stdout
+    Args:
+        filename: text
+    """
+    with open(filename, 'r') as file:
+        for lines in file:
+            print(lines.strip())
