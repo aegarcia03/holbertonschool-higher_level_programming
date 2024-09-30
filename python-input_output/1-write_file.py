ceab7247491ec392653b6d37383f0e60cb60ec67
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-09-30 15:11:28 +1000

    Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function that writes a string to a text file UTF8"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file UTF8
+    Args:
+        filename: text file UTF8
+        text: string to write in the file
+    Returns:
+        The number of characters written"""
+
+    with open(filename, 'w', encoding="UTF8") as file:
+        num_characters = file.write(text)
+        return num_characters
