842259d951572ce89ece25435ae9069b7e1c05f9
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-01 19:37:52 +1000

    Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module defines a "Student" class"""
+
+
+class Student:
+    """
+    A class used to define Student
+
+    Attributes:
+        first_name
+        last_name
+        age
+    Methods: ...
+    ____________"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first name, last name and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
