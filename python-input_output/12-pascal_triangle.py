563c4ac4c40da94a0409c43dea29bdc11d28176a
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-02 20:02:04 +1000

    Updating files

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,16 +6,23 @@ def pascal_triangle(n):
     """Returns a list of lists of integers
     Args:
         n: The number of rows"""
+    #Edge case: 
     if n <= 0:
         return []
-
+    # Initialize Pascal's triangle with the first row
     triangle = [[1]]
+    # Generate the rows from 1 to n - 1
     for i in range(1, n):
+        # Start each row with 1
         row = [1]
+        # Get the value of previos row 
         prev_row = triangle[i - 1]
+        # Append innerr elements (sum of two elements above)
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
+        # End each row with 1 
         row.append(1)
+        # Append the new row to the triangle 
         triangle.append(row)
 
     return triangle
