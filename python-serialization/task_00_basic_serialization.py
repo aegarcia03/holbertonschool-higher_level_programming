955f0327af08911038f19b0e08828582c0795ac5
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-03 20:21:15 +1000

    Pickling custom classes

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
-"""Defines a module that adds"""
+"""This module provides functions to serialize a Python dictionary to a JSON file
+and deserialize the JSON file back into a Python dictionary"""
 import json
 
 
 def serialize_and_save_to_file(data, filename):
+    """
+    Serializes a Python dictionary into a JSON format and saves it to a file.
+    
+    Parameters:
+        data (dict): The Python dictionary to be serialized.
+        filename (str): The name of the output JSON file where 
+        the serialized data will be saved.
+        """
     with open(filename, 'w') as file:
         json_str = json.dump(data, file)
 
 
 def load_and_deserialize(filename):
+    """
+    Loads data from a JSON file and deserializes it back into a Python dictionary.
+    
+    Parameters:
+        filename (str): The name of the input JSON file containing the serialized data.
+        
+    Returns:
+        dict: A Python dictionary deserialized from the JSON file.
+    """
     with open(filename, 'r') as file:
         return json.load(file)
---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""This module provides functions to serialize a Python dictionary
+and deserialize the JSON file with the pickle module"""
+import pickle
+
+
+class CustomObject:
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display the attributes of the object."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize the current instance to a file."""
+        try:
+            with open(filename, mode="wb") as file:
+                pickle_str = pickle.dump(self, file)
+        except IOERROR:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize an object from a file."""
+        try:
+            with open(filename, mode="rb") as file:
+                return pickle.load(file)
+        except FileNotFoundError or IOError:
+            return None
