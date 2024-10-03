f1b65e9eb07f27d519c20bd6ac45d6bb0f8d4058
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-03 19:52:36 +1000

    Basic Serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Defines a module that adds"""
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
-    with open(filename, mode="wb") as file:
-        pickle.dump(data, file)
+    with open(filename, 'w') as file:
+        json_str = json.dump(data, file)
 
 
 def load_and_deserialize(filename):
-    with open(filename, mode="rb") as file:
-        return pickle.load(file)
+    with open(filename, 'r') as file:
+        return json.load(file)
