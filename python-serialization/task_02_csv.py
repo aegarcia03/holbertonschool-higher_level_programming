00cf80eb0f3aaee2d8bd12c500a279a6b624dcb8
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-04 11:56:12 +1000

    CSV to JSON

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""This module reads data from CSV and writes the JSON data to a file"""
+import json
+import csv
+
+
+def convert_csv_to_json(csv_filename):
+    json_file = 'data.json'
+    data = {}
+    try:
+        with open(csv_filename, 'r', encoding="UTF8") as cvsf:
+            csv_reader = csv.DictReader(cvsf)
+            for row in csv_reader:
+                key = row['name']
+                data[key] = row
+
+        with open(json_file, 'w', encoding="UTF8") as jsonf:
+            json.dump(data, jsonf, indent=4)
+
+        return True
+    except FileNotFoundError:
+        return False
