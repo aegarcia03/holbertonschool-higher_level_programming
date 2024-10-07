5d98bfdc964598f37b91cef5e8c1e386cc5a2463
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-08 09:54:49 +1100

    Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -9,8 +9,7 @@ def fetch_and_print_posts():
 
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    print(f"Status code: {response.status_code}")
-
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         json_obj = response.json()
 
