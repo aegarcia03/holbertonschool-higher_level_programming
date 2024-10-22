9db719d850736ed0f94882020c5c57eb91e07cde
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-22 16:23:52 +1100

    Select the best

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- List all records with score >= 10 
-SELECT score, name FROM second_table where score >= 10 ORDER by score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER by score DESC;
