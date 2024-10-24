f849ae4eed223cf2054be7fb0a96b84dcc609d6a
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-24 11:47:46 +1100

    Cities of California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,2 @@
 -- Lists the cities of California found in database hbtn_0d_usa
-USE hbtn_0d_usa;
-
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California");
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California");
