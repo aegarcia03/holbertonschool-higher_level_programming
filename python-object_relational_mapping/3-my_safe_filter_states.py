4eaf2706249ad4e492dbb0367239b0a56f44c0ca
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-28 21:52:09 +1100

    SQL injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Nameless module for running SQL """
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+    )
+    query = """SELECT * FROM states WHERE name = '{state_name}' ORDER BY id ASC""".format(state_name = sys.argv[4])
+    c = db.cursor()
+    c.execute(query)
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
