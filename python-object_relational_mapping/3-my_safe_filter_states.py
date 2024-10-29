21cadc0df97dd8a733621eecc9335be3d759af2c
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-29 15:07:40 +1100

    SQL injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,10 +14,9 @@ if __name__ == "__main__":
         database=sys.argv[3],
     )
     query = """SELECT * FROM states WHERE BINARY
-                `name` = '{0}'
-                ORDER BY id ASC""".format(sys.argv[4])
+                name = %s ORDER BY id ASC"""
     c = db.cursor()
-    c.execute(query)
+    c.execute(query, ((sys.argv[4]), ))
     rows = c.fetchall()
 
     for row in rows:
