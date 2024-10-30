7bf3dc07d08673e8fc652d762aa55a0fb36c8dc0
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-30 15:25:50 +1100

    Cities in state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Define a State city for SQLAlchemy ORM"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from model_state import Base
+from sqlalchemy.orm import relationship
+
 
 class City(Base):
     """City class that maps to the cities table"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, foreign_key=states.id)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    state = relationship("State", back_populates="cities")
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,6 +2,7 @@
 """Define a State class for SQLAlchemy ORM"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
@@ -12,3 +13,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    cities = relationship("City", back_populates="state")
